d53073d77289e5a7c3439f6079302a798df37416
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2020-04-15 00:15:58 +0300

    Fix Home page

--- src/shared/routes/Home/index.tsx
+++ src/shared/routes/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { loadMockDataTwo } from 'Modules/MockDataTwo/actions/loadMockDataTwo';
-import { H1, Border, Button, Span } from '@antoniodcorrea/components';
+import { H1, Border, Button, Span, Layout } from '@antoniodcorrea/components';
 import './Home.less';
 
 interface Props {
@@ -13,9 +13,13 @@ const Home: React.FC<Props> = ({ updatedAt, loadMockDataTwo }) => {
   return (
     <div className="Home">
       <Border grow>
-        <H1>HOME PAGE</H1>
-        {updatedAt && <Span>Updated at: {updatedAt}</Span>}
-        <Button text="Load Mock Data Two" onClick={loadMockDataTwo} />
+        <Layout horizontal="center">
+          <H1 center grow>
+            HOME PAGE
+          </H1>
+          {updatedAt && <Span>Updated at: {updatedAt}</Span>}
+          <Button text="Load Mock Data Two" onClick={loadMockDataTwo} />
+        </Layout>
       </Border>
     </div>
   );
