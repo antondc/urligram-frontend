35fa88a0cd91360c5b03602585fbcfd68430246f
Antonio Díaz Correa <antonio.d.correa@gmail.com>
2020-04-17 10:20:17 +0300

    Add location change detector

--- src/shared/common/Layout/index.tsx
+++ src/shared/common/Layout/index.tsx
@@ -14,32 +14,45 @@ import './Layout.less';
 interface Props {
   languagesLoading: boolean;
   mockDataTwoLoading: boolean;
+  location: any;
 }
 
-const Layout: React.FC<Props> = ({ languagesLoading, mockDataTwoLoading }) => {
-  if (isBrowser) {
-    window.addEventListener('load', () => {
-      document.body.classList.remove('preload'); // Preventing animations on load
-      document.body.classList.add('isLoaded'); // Showing page on load
-    });
+class Layout extends React.Component<Props> {
+  componentDidMount() {
+    if (isBrowser) {
+      window.addEventListener('load', () => {
+        document.body.classList.remove('preload'); // Preventing animations on load
+        document.body.classList.add('isLoaded'); // Showing page on load
+      });
+    }
   }
-  const showLoader = languagesLoading || mockDataTwoLoading;
 
-  return (
-    <div className={'Layout'}>
-      <div className="Layout-modal">
-        <Fade time={150} mounted={showLoader}>
-          <SpinnerCircle />
-        </Fade>
+  componentDidUpdate = (prevProps) => {
+    if (this.props.location !== prevProps.location) {
+      // trigger new location push on Redux actions
+    }
+  };
+
+  render = () => {
+    const { languagesLoading, mockDataTwoLoading } = this.props;
+    const showLoader = languagesLoading || mockDataTwoLoading;
+
+    return (
+      <div className={'Layout'}>
+        <div className="Layout-modal">
+          <Fade time={150} mounted={showLoader}>
+            <SpinnerCircle />
+          </Fade>
+        </div>
+        <div className="Layout-content">
+          <Header />
+          <Route path="/:lang([a-z]{2})?" component={Main} />
+          <Footer />
+        </div>
       </div>
-      <div className="Layout-content">
-        <Header />
-        <Route path="/:lang([a-z]{2})?" component={Main} />
-        <Footer />
-      </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 const mapStateToProps = createStructuredSelector({
   languagesLoading: selectLanguageLoading,
