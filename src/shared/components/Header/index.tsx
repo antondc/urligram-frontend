f233aee617cfa875ea807a13b1f5bc0bf9ac37bc
antoniodcorrea <hello@antoniodiaz.me>
2021-01-04 01:53:41 +0300

    Revert to no issues II

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -28,7 +28,6 @@ const Header: React.FC<Props> = ({
   switchUserModal,
   currentLanguageSlug,
   switchLoginModal,
-
 }) => (
   <header>
     <Border className="Header" weight="thick">
@@ -51,6 +50,10 @@ const Header: React.FC<Props> = ({
         <span className="Header-bar">|</span>
         <A className="Header-link" href={'/' + currentLanguageSlug + '/control'} frontend>
           <H4>{currentGlossary.Lists}</H4>
+        </A>
+        <span className="Header-bar">|</span>
+        <A className="Header-link" href={'/' + currentLanguageSlug + '/bookmarks'} frontend>
+          <H4>{currentGlossary.Bookmarks}</H4>
         </A>
       </nav>
       <div className="Header-user">
---
+++ src/shared/routes/Bookmarks/Bookmarks.test.tsx
@@ -0,0 +1,49 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import { BookmarksUi } from './bookmarksUi';
+
+const props = {
+  bookmarks: [
+    {
+      id: 1,
+      title: 'My first bookmark',
+      url: 'http://example.com/path',
+      img: 'https://picsum.photos/id/123/300/200',
+      tags: [{ id: 2, name: 'Media' }],
+      statistics: {
+        absoluteVote: 0,
+        timesVoted: 0,
+        averageVote: 0,
+        timesBookmarked: 0,
+        vote: false,
+      },
+    },
+    {
+      id: 2,
+      title: 'A second bookmark',
+      url: 'http://example.com/path',
+      img: 'https://picsum.photos/id/21/300/200',
+      tags: [{ id: 4, name: 'Long text' }],
+      statistics: {
+        absoluteVote: 0,
+        timesVoted: 0,
+        averageVote: 0,
+        timesBookmarked: 0,
+        vote: false,
+      },
+    },
+  ],
+};
+
+describe('BookmarksUi', () => {
+  const wrapper = shallow(<BookmarksUi {...props} />);
+
+  test('component renders', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('has a ".Main" wrapper', () => {
+    expect(wrapper.find('.Bookmarks')).toHaveLength(1);
+  });
+});
