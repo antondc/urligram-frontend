d9a65a08196e953b7af322ecaf4c0c82397b3cb9
antoniodcorrea <hello@antoniodiaz.me>
2021-01-31 12:51:39 +0300

    Fix tests

--- src/shared/routes/ListsUser/ListsUser.test.tsx
+++ src/shared/routes/ListsUser/ListsUser.test.tsx
@@ -5,27 +5,7 @@ import { ListsUser as ListsUserUi } from './ListsUser';
 
 const props = {
   listsIds: [1, 2],
-  popularLists: [
-    {
-      id: 3,
-      name: 'Third list',
-      image: '',
-      description: 'More links',
-      isPrivate: false,
-      userId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-      createdAt: '2021-01-08T17:22:29.000Z',
-      updatedAt: '2021-01-08T17:22:29.000Z',
-      members: [
-        {
-          id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-          name: 'User3',
-          userRole: 'admin',
-        },
-      ],
-      bookmarksIds: [1, 2, 3],
-      tags: [],
-    },
-  ],
+  popularLists: [],
   loading: true,
 };
 
@@ -36,7 +16,7 @@ describe('ListsUserUi', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  test('has a ".Lists" wrapper', () => {
-    expect(wrapper.find('.Lists')).toHaveLength(1);
+  test('has a ".ListsUser" wrapper', () => {
+    expect(wrapper.find('.ListsUser')).toHaveLength(1);
   });
 });
--- src/shared/routes/ListsVisitor/ListsVisitor.test.tsx
+++ src/shared/routes/ListsVisitor/ListsVisitor.test.tsx
@@ -5,27 +5,7 @@ import { ListsVisitor as ListsVisitorUi } from './ListsVisitor';
 
 const props = {
   listsIds: [1, 2],
-  popularLists: [
-    {
-      id: 3,
-      name: 'Third list',
-      image: '',
-      description: 'More links',
-      isPrivate: false,
-      userId: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-      createdAt: '2021-01-08T17:22:29.000Z',
-      updatedAt: '2021-01-08T17:22:29.000Z',
-      members: [
-        {
-          id: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-          name: 'User3',
-          userRole: 'admin',
-        },
-      ],
-      bookmarksIds: [1, 2, 3],
-      tags: [],
-    },
-  ],
+  popularLists: [],
   loading: true,
 };
 
@@ -37,6 +17,6 @@ describe('ListsVisitorUi', () => {
   });
 
   test('has a ".ListsVisitor" wrapper', () => {
-    expect(wrapper.find('.Lists')).toHaveLength(1);
+    expect(wrapper.find('.ListsVisitor')).toHaveLength(1);
   });
 });
