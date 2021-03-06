7c26d7d76524d4b0d8cef88dc3485a417594fb3e
antoniodcorrea <hello@antoniodiaz.me>
2021-03-06 13:27:09 +0300

    LinkRow Styled

---
+++ src/shared/components/LinkRow/BookmarkRow.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { withKnobs } from '@storybook/addon-knobs';
+import { LinkRow } from './LinkRow';
+
+export default {
+  component: LinkRow,
+  title: 'LinkRow',
+  decorators: [withKnobs],
+};
+
+const noop = () => {};
+
+const props = {
+  id: 1,
+  linkId: 12,
+  title: 'Some title text a bit long',
+  url: 'https://en.wikipedia.org/wiki/Khanty',
+  img: 'https://picsum.photos/id/123/300/200',
+  tags: [
+    { id: 1, name: 'Cool' },
+    { id: 2, name: 'Interesting' },
+  ],
+  statistics: {
+    absoluteVote: 0,
+    timesVoted: 0,
+    averageVote: 0,
+    timesBookmarked: 0,
+    vote: false,
+    loading: false,
+  },
+  onVote: noop,
+};
+
+export const Default: React.ReactNode = () => <LinkRow {...props} />;
--- src/shared/components/LinkRow/LinkRow.tsx
+++ src/shared/components/LinkRow/LinkRow.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import A from 'Components/A';
-import { LinkState } from 'Modules/Links/links.types';
-import { Border, Circle, Edit, Hr, Private, Span, Tag, Vote } from '@antoniodcorrea/components';
+import { LinkState } from 'Modules/Links/Links.types';
+import { A, Border, Circle, Edit, Hr, Private, Span, SvgIcon, Tag, Vote } from '@antoniodcorrea/components';
 
 import './LinkRow.less';
 
@@ -10,61 +9,59 @@ interface LinkRow extends LinkState {
   onVote: (vote: boolean | null) => void;
 }
 
-export const LinkRow: React.FC<LinkRow> = ({ id, title, url, tags = [], img, statistics, onVote }) => (
+export const LinkRow: React.FC<LinkRow> = ({ id, title, url, tags = [], statistics, onVote }) => (
   <Border grow className="LinkRow" data-test-id="LinkRow" key={id}>
     <div className="LinkRow-left">
-      <div className="LinkRow-leftTop">
-        <div className="LinkRow-icons">
-          <Circle size="micro" className="LinkRow-icon" />
-          <Private
-            size="micro"
-            className="LinkRow-icon LinkRow-iconHover"
-            onClick={() => {
-              alert('Private');
-            }}
-          />
-          <Edit
-            size="micro"
-            className="LinkRow-icon LinkRow-iconHover"
-            onClick={() => {
-              alert('Edit');
-            }}
-          />
-        </div>
-        <Hr spacer size="micro" />
-        <Span bold className="LinkRow-title">
-          {title}
+      <div className="LinkRow-icons">
+        <Circle size="micro" className="LinkRow-icon" />
+        <Private
+          size="micro"
+          className="LinkRow-icon LinkRow-iconHover"
+          onClick={() => {
+            alert('Private');
+          }}
+        />
+        <Edit
+          size="micro"
+          className="LinkRow-icon LinkRow-iconHover"
+          onClick={() => {
+            alert('Edit');
+          }}
+        />
+      </div>
+      <Span bold className="LinkRow-title">
+        {title}
+      </Span>
+      <A href={url} targetBlank>
+        <Span className="LinkRow-url" size="small">
+          {url}
         </Span>
-        <Hr spacer size="zero" />
-        <A href={url} targetBlank>
-          <Span size="small">{url}</Span>
+      </A>
+    </div>
+
+    <div className="LinkRow-center">
+      {tags?.map((item) => (
+        <A className="LinkRow-tag" href={`/links?filter[tags][]=${item.name}`} key={item.id} styled={false} frontend>
+          <Tag size="nano" variant="simple">
+            {item.name}
+          </Tag>
         </A>
-      </div>
-      <div className="LinkRow-leftBottom">
-        {tags?.map((item) => (
-          <A className="LinkRow-tag" href={`/links?filter[tags][]=${item.name}`} key={item.id} styled={false} frontend>
-            <Tag size="small">{item.name}</Tag>
-          </A>
-        ))}
-      </div>
+      ))}
     </div>
     <div className="LinkRow-right">
-      <img className="LinkRow-image" src={img} />
-      <div className="LinkRow-rightEnd">
-        <Vote className="LinkRow-vote" vote={statistics?.vote} changeVote={onVote} loading={statistics?.loading} />
-        <div className="LinkRow-stats">
-          <div className="LinkRow-stat">
-            <Span size="nano" className="LinkRow-statIcon">
-              ▲
-            </Span>
-            32
-          </div>
-          <div className="LinkRow-stat">
-            <Span size="nano" className="LinkRow-statIcon">
-              ⚭
-            </Span>
-            124
-          </div>
+      <Vote className="LinkRow-vote" vote={statistics?.vote} changeVote={onVote} loading={statistics?.loading} />
+      <div className="LinkRow-stats">
+        <div className="LinkRow-stat">
+          <Span size="small" className="LinkRow-statIcon">
+            ▲
+          </Span>
+          32
+        </div>
+        <div className="LinkRow-stat">
+          <Span size="small" className="LinkRow-statIcon">
+            ⚭
+          </Span>
+          124
         </div>
       </div>
     </div>
