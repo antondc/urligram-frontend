81a49767138be87c3ca97e157c43140e71f8e102
antoniodcorrea <hello@antoniodiaz.me>
2021-03-06 13:38:18 +0300

    LinkRow BookmarkRow fix imports

--- src/shared/components/LinkRow/LinkRow.tsx
+++ src/shared/components/LinkRow/LinkRow.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import A from 'Components/A';
 import { LinkState } from 'Modules/Links/Links.types';
-import { A, Border, Circle, Edit, Hr, Private, Span, SvgIcon, Tag, Vote } from '@antoniodcorrea/components';
+import { Border, Circle, Edit, Private, Span, Tag, Vote } from '@antoniodcorrea/components';
 
 import './LinkRow.less';
 
