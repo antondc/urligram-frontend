a5310a68c85398f6853ee602940520a6f4810322
antoniodcorrea <hello@antoniodiaz.me>
2021-03-31 11:20:19 +0300

    Footer add spacer

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -9,7 +9,7 @@ import { selectCurrentLanguageSlug } from 'Modules/Languages/selectors/selectCur
 import { selectCurrentPathname } from 'Modules/Routes/selectors/selectCurrentPathname';
 import { switchLanguagesModal } from 'Modules/Ui/actions/switchLanguagesModal';
 import { selectUiLanguagesModalMounted } from 'Modules/Ui/selectors/selectUiLanguagesModalMounted';
-import { Border, Fade, Span } from '@antoniodcorrea/components';
+import { Border, Fade, Hr, Span } from '@antoniodcorrea/components';
 
 import './Footer.less';
 
@@ -26,44 +26,47 @@ const Footer: React.FC<Props> = ({
   currentPathName,
   switchLanguagesModal,
 }) => (
-  <Border className="Footer" weight="thick">
-    <div className="Footer-section">
-      <A href="sign-up" frontend>
-        <Span bold>Sign up</Span>
-      </A>
-    </div>
-    <div className="Footer-section">
-      <A href="about" frontend>
-        <Span bold>About</Span>
-      </A>
-    </div>
-    <div className="Footer-section">
-      <A href="" frontend>
-        <Span bold>Disclaimer</Span>
-      </A>
-    </div>
-    <div className="Footer-section">
-      <A href="" frontend>
-        <Span bold>FAQ</Span>
-      </A>
-    </div>
-    <div className="Footer-section">
-      <A href="" frontend>
-        <Span bold>Download</Span>
-      </A>
-    </div>
-    <div className="Footer-section">
-      <A href="" frontend>
-        <Span bold>Contact</Span>
-      </A>
-    </div>
-    <div className="Footer-section Footer-lastSection">
-      <Fade mounted={uiLanguagesModalMounted}>
-        <LanguagesSwitch />
-      </Fade>
-      <LanguageItem lang={currentLanguageSlug} onClick={switchLanguagesModal} href={currentPathName} />
-    </div>
-  </Border>
+  <>
+    <Hr spacer />
+    <Border className="Footer" weight="thick">
+      <div className="Footer-section">
+        <A href="sign-up" frontend>
+          <Span bold>Sign up</Span>
+        </A>
+      </div>
+      <div className="Footer-section">
+        <A href="about" frontend>
+          <Span bold>About</Span>
+        </A>
+      </div>
+      <div className="Footer-section">
+        <A href="" frontend>
+          <Span bold>Disclaimer</Span>
+        </A>
+      </div>
+      <div className="Footer-section">
+        <A href="" frontend>
+          <Span bold>FAQ</Span>
+        </A>
+      </div>
+      <div className="Footer-section">
+        <A href="" frontend>
+          <Span bold>Download</Span>
+        </A>
+      </div>
+      <div className="Footer-section">
+        <A href="" frontend>
+          <Span bold>Contact</Span>
+        </A>
+      </div>
+      <div className="Footer-section Footer-lastSection">
+        <Fade mounted={uiLanguagesModalMounted}>
+          <LanguagesSwitch />
+        </Fade>
+        <LanguageItem lang={currentLanguageSlug} onClick={switchLanguagesModal} href={currentPathName} />
+      </div>
+    </Border>
+  </>
 );
 
 const mapStateToProps = createStructuredSelector({
