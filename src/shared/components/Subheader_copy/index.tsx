5c4fef31c419647f5f7445af850ee04d9b459aeb
antoniodcorrea <hello@antoniodiaz.me>
2021-06-03 12:52:59 +0300

    Fix case conflict

---
+++ src/shared/components/Subheader_copy/index.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectCurrentLanguageSlug } from 'Modules/Languages/selectors/selectCurrentLanguageSlug';
+import { HttpClient } from 'Services/HttpClient';
+import { LocaleFormattedDate } from 'Tools/utils/Date/localeFormattedDate';
+import { Subheader_ as SubheaderUi } from './Subheader_';
+
+import './Subheader_.less';
+
+const Subheader_: React.FC = () => {
+  const [weatherString, setWheatherString] = useState<string>(undefined);
+  const currentLanguageSlug = useSelector(selectCurrentLanguageSlug);
+  const dateNowMs = Math.floor(Date.now() / 1000);
+  const date = new LocaleFormattedDate({ unixTime: dateNowMs, locale: currentLanguageSlug });
+  const formattedDate = date.getLocaleFormattedDate();
+
+  useEffect(() => {
+    const asyncFunction = async () => {
+      try {
+        const httpClient = new HttpClient({ credentials: false });
+        const weather: string = await httpClient.publicInstance.get(
+          'https://wttr.in/?format=%l:+·+%c+·+%t+·+%m+·+%P+·+%w'
+        );
+        setWheatherString(weather);
+      } catch (error) {
+        console.log('Subheader_.useEffect.catch error: ', error);
+      }
+    };
+
+    asyncFunction();
+  }, []);
+
+  return <SubheaderUi formattedDate={formattedDate} weatherString={weatherString} />;
+};
+
+export default Subheader_;
