ca841b017fe719e6742c2a229c2f63495f4f23cb
antoniodcorrea <hello@antoniodiaz.me>
2021-06-08 23:52:55 +0300

    Refactor Layout to hooks

--- src/shared/typescript/globals.d.ts
+++ src/shared/typescript/globals.d.ts
@@ -6,3 +6,13 @@ declare module '*.svg' {
   const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default value;
 }
+
+declare namespace React {
+  function memo<Props extends Record<string, any>>(
+    Component: FC<Props>,
+    propsAreEqual?: (
+      prevProps: Readonly<PropsWithChildren<Props>>,
+      nextProps: Readonly<PropsWithChildren<Props>>
+    ) => boolean
+  ): NamedExoticComponent<PropsWithChildren<Props>>;
+}
