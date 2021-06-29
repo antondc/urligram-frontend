980ae78f3920accb6aac904e30797bd75812fcde
antoniodcorrea <hello@antoniodiaz.me>
2021-06-30 01:14:10 +0300

    Modify HttpClient

--- src/shared/services/HttpClient.ts
+++ src/shared/services/HttpClient.ts
@@ -6,6 +6,7 @@ import { QueryStringWrapper } from './QueryStringWrapper';
 interface Options {
   timeout?: number;
   credentials?: boolean;
+  contentType?: string;
 }
 
 export class HttpClient {
@@ -21,13 +22,19 @@ export class HttpClient {
       }),
     });
 
-    axiosInstance.defaults.timeout = options?.timeout;
+    if (!!options?.timeout) axiosInstance.defaults.timeout = options?.timeout;
     axiosInstance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     axiosInstance.defaults.paramsSerializer = this.paramsSerializer;
     axiosInstance.defaults.withCredentials = credentials;
 
     axiosInstance.interceptors.response.use(
-      (response) => response.data,
+      (response) => {
+        if (!!options?.contentType && !response?.headers['content-type']?.includes(options?.contentType)) {
+          throw new Error("Received response doesn't match provided content-type");
+        }
+
+        return response.data;
+      },
       (err) => Promise.reject(err)
     );
 
