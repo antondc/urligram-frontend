b90ac076a544195bab915e424c8d4164a8eb2bde
antoniodcorrea <hello@antoniodiaz.me>
2021-07-07 01:25:59 +0300

    BookmarkRow desktop

--- src/shared/components/BookmarkRow/BookmarkRow.tsx
+++ src/shared/components/BookmarkRow/BookmarkRow.tsx
@@ -5,7 +5,6 @@ import BookmarkActions from 'Components/BookmarkActions';
 import BookmarkLists from 'Components/BookmarkLists';
 import { BookmarkState } from 'Modules/Bookmarks/bookmarks.types';
 import { EditCircle, Private, Space, Tag, Tooltip, Vote } from 'Vendor/components';
-import { stringToDashCase } from '../../tools/utils/string/stringToDashCase';
 import { RenderInPortal } from '../Portal';
 
 import './BookmarkRow.less';
@@ -34,42 +33,37 @@ export const BookmarkRow: React.FC<Partial<BookmarkRow>> = ({
     data-test-id="BookmarkRow"
     key={bookmark?.id}
   >
-    <div className="BookmarkRow-main">
-      <div className="BookmarkRow-mainTitle">
-        <img className="BookmarkRow-favicon" src={bookmark?.favicon} />
-        <Space />
-        <A className="BookmarkRow-title" href={bookmark?.url} targetBlank styled={false}>
-          {bookmark?.title}
-        </A>
-      </div>
-      <div className="BookmarkRow-mainBottom">
-        {!!bookmark?.statistics?.timesBookmarked && (
-          <>
-            Shared by
-            <Space />
-            {bookmark?.statistics?.timesBookmarked}
-            <Space />
-            user
-            <span className="BookmarkRow-mainBottomNumber">
-              {bookmark?.statistics?.timesBookmarked !== 1 ? 's' : ''}
-            </span>
-          </>
-        )}
-        {!!bookmark?.statistics?.timesBookmarked && !!bookmark?.statistics?.absoluteVote && (
-          <>
-            <Space />·<Space />
-          </>
-        )}
-        {!!bookmark?.statistics?.absoluteVote && (
-          <>
-            Total votes
-            <Space />
-            <span className="BookmarkRow-mainBottomNumber">{bookmark?.statistics?.absoluteVote}</span>
-          </>
-        )}
-        <Space />·<Space />
-        Created at {createdAtFormatted}
-      </div>
+    <div className="BookmarkRow-title">
+      <img className="BookmarkRow-favicon" src={bookmark?.favicon} />
+      <A className="BookmarkRow-link" href={bookmark?.url} targetBlank styled={false}>
+        {bookmark?.title}
+      </A>
+    </div>
+    <div className="BookmarkRow-details">
+      {!!bookmark?.statistics?.timesBookmarked && (
+        <>
+          Shared by
+          <Space />
+          {bookmark?.statistics?.timesBookmarked}
+          <Space />
+          user
+          <span>{bookmark?.statistics?.timesBookmarked !== 1 ? 's' : ''}</span>
+        </>
+      )}
+      {!!bookmark?.statistics?.timesBookmarked && !!bookmark?.statistics?.absoluteVote && (
+        <>
+          <Space />·<Space />
+        </>
+      )}
+      {!!bookmark?.statistics?.absoluteVote && (
+        <>
+          Total votes
+          <Space />
+          {bookmark?.statistics?.absoluteVote}
+        </>
+      )}
+      <Space />·<Space />
+      Created at {createdAtFormatted}
     </div>
     <div className="BookmarkRow-tags">
       {bookmark?.tags?.map((item) => (
@@ -87,30 +81,41 @@ export const BookmarkRow: React.FC<Partial<BookmarkRow>> = ({
       ))}
     </div>
     <div className="BookmarkRow-icons">
-      {!!bookmark?.isPrivate && <Private size="micro" className="BookmarkRow-icon BookmarkRow-private" />}
-      {!!sessionUserBookmarkedLink && <EditCircle className="BookmarkRow-icon" size="micro" onClick={onEdit} />}
-      {!!sessionUserBookmarkedLink && (
-        <div className="BookmarkRow-icon BookmarkRow-iconLists">
-          <BookmarkLists bookmarkId={bookmark?.id} />
-        </div>
-      )}
-      <Vote
-        className="BookmarkRow-iconVote"
-        vote={bookmark?.statistics?.vote}
-        loading={bookmark?.statistics?.loading}
-        changeVote={onVote}
-      />
-      <RenderInPortal>
-        <Tooltip parentElementId={`BookmarkRow-averageVote-${bookmark?.id}`} content="Users in this list" delay={0.5} />
-      </RenderInPortal>
-      <div className="BookmarkRow-averageVote" id={`BookmarkRow-averageVote-${bookmark?.id}`}>
-        {bookmark?.statistics?.absoluteVote || 0}
+      <div className="BookmarkRow-iconsFirst">
+        {!!bookmark?.isPrivate && <Private size="micro" className="BookmarkRow-icon BookmarkRow-private" />}
+        {!!sessionUserBookmarkedLink && (
+          <EditCircle className="BookmarkRow-icon BookmarkRow-editButton" size="micro" onClick={onEdit} />
+        )}
+        {!!sessionUserBookmarkedLink && (
+          <div className="BookmarkRow-icon BookmarkRow-iconLists">
+            <BookmarkLists bookmarkId={bookmark?.id} />
+          </div>
+        )}
+
+        <BookmarkActions
+          className="BookmarkRow-icon BookmarkRow-actionButton"
+          linkId={bookmark?.linkId}
+          bookmarkId={bookmark?.id}
+        />
       </div>
-      <BookmarkActions
-        className="BookmarkRow-icon BookmarkRow-actionButton"
-        linkId={bookmark?.linkId}
-        bookmarkId={bookmark?.id}
-      />
+      <div className="BookmarkRow-iconsSecond">
+        <RenderInPortal>
+          <Tooltip
+            parentElementId={`BookmarkRow-averageVote-${bookmark?.id}`}
+            content="Users in this list"
+            delay={0.5}
+          />
+        </RenderInPortal>
+        <div className="BookmarkRow-averageVote" id={`BookmarkRow-averageVote-${bookmark?.id}`}>
+          {bookmark?.statistics?.absoluteVote || 0}
+        </div>
+        <Vote
+          className="BookmarkRow-iconVote"
+          vote={bookmark?.statistics?.vote}
+          loading={bookmark?.statistics?.loading}
+          changeVote={onVote}
+        />
+      </div>
     </div>
   </div>
 );
