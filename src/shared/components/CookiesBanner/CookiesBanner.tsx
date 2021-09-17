eeabb0de3b376cc65cd8bb7a0328ddc5ec402237
Antonio Díaz Correa <hello@antoniodiaz.me>
2021-09-17 16:42:01 +0300

    CookiesBanner

--- src/shared/components/CookiesBanner/CookiesBanner.tsx
+++ src/shared/components/CookiesBanner/CookiesBanner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import A from 'Components/A';
-import { AnimateHeight, Button, Hr, Space, Span } from 'Vendor/components';
+import { AnimateHeight, Button, Space } from 'Vendor/components';
 
 import './CookiesBanner.less';
 
@@ -15,20 +15,15 @@ export const CookiesBanner: React.FC<Props> = ({ onAccept, accepted, show }) => 
   <AnimateHeight mounted={show} speed="fastest" ease={[1, 0.02, 0.83, 1.15]}>
     <div className={'CookiesBanner' + (accepted ? ' CookiesBanner--accepted' : '')}>
       <div className="CookiesBanner-text">
-        <Span className="CookiesBanner-textSpan CookiesBanner-textSpanTitle" weight="semiBold" size="medium">
-          We use cookies
-        </Span>
-        <Hr spacer size="zero" />
-        <Span className="CookiesBanner-textSpan CookiesBanner-textSpanBody" size="small">
+        <div className="CookiesBanner-textSpan CookiesBanner-textSpanTitle">We use cookies</div>
+        <span className="CookiesBanner-textSpan CookiesBanner-textSpanBody">
           We use cookies to improve the user experience of this site. You can find all the info at
           <Space />
           <A href="/policy" frontend underlined>
-            <Span weight="semiBold" size="small">
-              the policy page
-            </Span>
+            <span>the policy page</span>
           </A>
           .
-        </Span>
+        </span>
       </div>
       <div className="CookiesBanner-buttons">
         <Button className="CookiesBanner-accept" text="Accept" onClick={onAccept} success={accepted} />
