37b983d2c62e7c582fabbe835402fdef7a29e0cd
Antonio Díaz Correa <hello@antoniodiaz.me>
2021-09-18 14:14:38 +0300

    Clean Redux Routes

--- src/shared/redux/modules/Routes/selectors/selectCurrentRouteQueryParamSort.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import get from 'lodash/get';
-
-import { RouteState } from '../routes.types';
-import { selectCurrentRoute } from './selectCurrentRoute';
-
-export const selectCurrentRouteQueryParamSort = createSelector(selectCurrentRoute, (currentRoute: RouteState): string =>
-  get(currentRoute, 'queryParams.sort', '')
-);
--- src/shared/redux/modules/Routes/selectors/selectPreviousRoute.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-import { RoutesState, RouteState } from './../routes.types';
-import { selectRoutes } from './selectRoutes';
-
-export const selectPreviousRoute = createSelector(
-  selectRoutes,
-  (routes: RoutesState): RouteState => routes?.history[routes?.history?.length - 2]
-);
