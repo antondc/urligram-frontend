d3c91ad9d5672fb54af9fecacb28420fe70b0604
antondc <hello@antoniodiaz.me>
2023-03-21 22:35:39 +0100

    Update docs

--- src/shared/pages/Docs/Extension.tsx
+++ src/shared/pages/Docs/Extension.tsx
@@ -19,28 +19,32 @@ export const Extension: React.FC<Props> = ({ appName, uiScreenTypeIsMobile }) =>
       on {appName} along with tags and lists, all without leaving the current page.
     </p>
     <h4 className="Docs-h4">How do I install the extension?</h4>
-    <p>
-      First, visit the appropriate official store of your browser by clicking the link for{' '}
-      <a
-        className={'Docs-link' + (uiScreenTypeIsMobile ? ' Docs-link--disabled' : '')}
-        href={FIREFOX_EXTENSION_URL}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Firefox
-      </a>{' '}
-      <Firefox className="Docs-iconFirefox" /> or{' '}
-      <a
-        className={'Docs-link' + (uiScreenTypeIsMobile ? ' Docs-link--disabled' : '')}
-        href={CHROME_EXTENSION_URL}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Chrome
-      </a>{' '}
-      <Chrome className="Docs-iconChrome" />. There you will be able to install it following the store instructions.
-      Remember that you will need to be logged in in order to save bookmarks into your account.
-    </p>
+    {uiScreenTypeIsMobile ? (
+      <p>
+        We have an extension available for Desktop browsers. As you are currently on mobile you wont be able to download
+        it. If you want to use it, please return in a desktop browser to this same page and follow the instructions to
+        download it.
+      </p>
+    ) : (
+      <p>
+        First, visit the appropriate official store of your browser by clicking the link for{' '}
+        <span className="Docs-linkWithIcon">
+          <a className="Docs-link" href={FIREFOX_EXTENSION_URL} target="_blank" rel="noreferrer">
+            Firefox
+          </a>
+          <Firefox className="Docs-iconFirefox" />
+        </span>{' '}
+        or{' '}
+        <span className="Docs-linkWithIcon">
+          <a className="Docs-link" href={CHROME_EXTENSION_URL} target="_blank" rel="noreferrer">
+            Chrome
+          </a>
+          <Chrome className="Docs-iconChrome" />
+        </span>
+        . There you will be able to install it following the store instructions. Remember that you will need to be
+        logged in in order to save bookmarks into your account.
+      </p>
+    )}
     <h4 className="Docs-h4">Does the extension share data with third parties?</h4>
     <p>No, {appName} does not share any data with other companies.</p>
   </div>
