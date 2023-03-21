ba1fa8603bccc9994c3bd27d046e411d89a1af14
antondc <hello@antoniodiaz.me>
2023-03-21 22:17:56 +0100

    Enable redux on production

--- src/shared/redux/index.ts
+++ src/shared/redux/index.ts
@@ -32,9 +32,8 @@ declare global {
 // Configuration for Redux devtools
 // Exclude production version
 const reduxDevToolsWrapper =
-  (process.env.NODE_ENV !== 'production' &&
-    typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  /* process.env.NODE_ENV !== 'production' && */
+  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const storeFactory = (initialState = {}): any =>
