661d0580b3930f75a7e558fd448b6198c549c584
antoniodcorrea <hello@antoniodiaz.me>
2023-09-11 20:56:53 +0200

    GR-16 update react render methods

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Router } from 'react-router-dom';
 
@@ -7,6 +6,7 @@ import Layout from 'Common/Layout';
 import storeFactory from 'Redux/.';
 import config from 'Root/config.test.json';
 import history from 'Services/History';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
 interface CustomWindow extends Window {
   __PRELOADED_STATE__: any;
@@ -18,17 +18,18 @@ const preloadedState = window.__PRELOADED_STATE__ || {};
 delete window.__PRELOADED_STATE__; // Allow state to be garbage collected: https://redux.js.org/recipes/server-rendering#clientjs
 
 const store = storeFactory(preloadedState);
-const hydrateOrRender = config.ENABLE_ISOMORPHISM ? hydrate : render;
+const container = document.getElementById('app');
+const hydrateOrRender = config.ENABLE_ISOMORPHISM ? hydrateRoot : createRoot;
 
 const renderApp = () =>
   // Sending the Router with Route component; Layout component sent inside render method to insert data
   hydrateOrRender(
+    container,
     <Provider store={store}>
       <Router history={history}>
         <Route path="/" component={Layout} />
       </Router>
-    </Provider>,
-    document.getElementById('app')
+    </Provider>
   );
 
 if (module.hot) module.hot.accept('Common/Layout', renderApp); // HMR
